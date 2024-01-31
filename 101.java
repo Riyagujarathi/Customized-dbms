d4bed0c83c72f57f065400deff399f491b331c51
Riya Pankaj Gujarathi <88250293+Riyagujarathi@users.noreply.github.com>
2024-01-31 22:19:52 +0530

    Update 101.java

--- 101.java
+++ 101.java
@@ -1,151 +1,120 @@
-import java.lang.*;
 import java.util.*;
 
-// create table student(RID int, Name varchar(255), Salary int);
-// Database table / schema
-class Student
-{
+class Student {
     public int RID;
     public String Name;
     public int Salary;
 
     private static int Generator;
 
-    static
-    {
+    static {
         Generator = 0;
     }
 
-    public Student(String str, int value)
-    {
+    public Student(String str, int value) {
         this.RID = ++Generator;
         this.Name = str;
         this.Salary = value;
     }
 
-    public void DisplayData()
-    {
+    public void DisplayData() {
         System.out.println(this.RID + "\t" + this.Name + "\t" + this.Salary);
     }
 }
 
-class DBMS
-{
-   public LinkedList <Student> lobj;
+class DBMS {
+    public LinkedList<Student> lobj;
 
-    public DBMS()
-    {
+    public DBMS() {
         lobj = new LinkedList<>();
     }
 
-    // Insert into student Piyush 1000;
-    // select * from student
-    public void StartDBMS()
-    {
+    public void StartDBMS() {
         Scanner scanobj = new Scanner(System.in);
 
-        System.out.println(" customised DBMS started succesfully....");
+        System.out.println("Customized DBMS started successfully....");
         String Query = "";
 
-        while(true)
-        {
+        while (true) {
             System.out.print("DBMS console >");
             Query = scanobj.nextLine();
 
             String tokens[] = Query.split(" ");
             int QuerySize = tokens.length;
 
-            if(QuerySize == 1)
-            {
-                if("Help".equals(tokens[0]))
-                {
-                    System.out.println("This application is used to demonstrates the customised DBMS");
+            if (QuerySize == 1) {
+                if ("Help".equals(tokens[0])) {
+                    System.out.println("This application is used to demonstrate the customized DBMS");
                     System.out.println("Exit : Terminate DBMS");
                     System.out.println("Display all data : select * from student");
                     System.out.println("Insert data : Insert into student Name Salary");
-                }
-                else if("Exit".equals(tokens[0]))
-                {
+                    System.out.println("Delete data by RID : delete from student where RID");
+                    System.out.println("Delete data by Name: delete from student where Name ");
+                } else if ("Exit".equals(tokens[0])) {
                     System.out.println("Thank you for using customize DBMS");
                     break;
                 }
-            }
-            else if(QuerySize == 2)
-            {
-
-            }
-            else if(QuerySize == 4)
-            {
-                if("select".equals(tokens[0]))
-                {
-                    if("*".equals(tokens[1]))
-                    {
-                        DisplayAll();
-                    }
+            } else if (QuerySize == 2) {
+                // Handle operations with 2 tokens if needed
+            } else if (QuerySize == 4) {
+                if ("select".equals(tokens[0]) && "*".equals(tokens[1])) {
+                    DisplayAll();
                 }
-            }
-            else if(QuerySize == 5)
-            {
+            } else if (QuerySize == 5) {
                 // Insert into student Piyush 1000;
-                if("Insert".equals(tokens[0]))
-                {
-                     InsertData(tokens[3],Integer.parseInt(tokens[4]));
+                if ("Insert".equals(tokens[0])) {
+                    InsertData(tokens[3], Integer.parseInt(tokens[4]));
                 }
+                
             }
-
+            else if(QuerySize == 7)
+			{
+				//delete from student where rid = ?
+				if( ("delete".equals(tokens[0])) && ("from".equals(tokens[1])) && ("student".equals(tokens[2])) && ("where".equals(tokens[3])) && ("rid".equals(tokens[4])) && ("=".equals(tokens[5])))
+				{
+					DeleteSpecific(Integer.parseInt(tokens[6]));
+				}
+                else if( ("delete".equals(tokens[0])) && ("from".equals(tokens[1])) && ("student".equals(tokens[2])) && ("where".equals(tokens[3])) && ("name".equals(tokens[4])) && ("=".equals(tokens[5])))
+				{
+					DeleteSpecific(tokens[6]);;
+				}
+            }
         }
     }
 
-    // Insert into query
-    public void InsertData(String str, int value)
-    {
-        Student sobj = new Student(str,value);
+    public void InsertData(String str, int value) {
+        Student sobj = new Student(str, value);
         lobj.add(sobj);
     }
 
-    // Display all without condition
-    public void DisplayAll()
-    {
-        for(Student sref : lobj)
-        {
+    public void DisplayAll() {
+        for (Student sref : lobj) {
             sref.DisplayData();
         }
     }
 
-    // Display by RID
-    public void DisplaySpecific(int rid)
-    {
-         for(Student sref : lobj)
-        {
-            if(sref.RID == rid)
-            {
+    public void DisplaySpecific(int rid) {
+        for (Student sref : lobj) {
+            if (sref.RID == rid) {
                 sref.DisplayData();
                 break;
             }
         }
     }
 
-    // Display by name
-    public void DisplaySpecific(String str)
-    {
-         for(Student sref : lobj)
-        {
-            if(str.equals(sref.Name))
-            {
+    public void DisplaySpecific(String str) {
+        for (Student sref : lobj) {
+            if (str.equals(sref.Name)) {
                 sref.DisplayData();
             }
         }
     }
 
-    // Delete by RID
-    public void DeleteSpecific(int rid)
-    {
+    public void DeleteSpecific(int rid) {
         int index = 0;
 
-        for(Student sref : lobj)
-        {
-            if(sref.RID == rid)
-            {
+        for (Student sref : lobj) {
+            if (sref.RID == rid) {
                 lobj.remove(index);
                 break;
             }
@@ -153,15 +122,11 @@ class DBMS
         }
     }
 
-    // Delete by Name
-    public void DeleteSpecific(String str)
-    {
+    public void DeleteSpecific(String str) {
         int index = 0;
 
-        for(Student sref : lobj)
-        {
-            if(str.equals(sref.Name))
-            {
+        for (Student sref : lobj) {
+            if (str.equals(sref.Name)) {
                 lobj.remove(index);
                 break;
             }
@@ -169,15 +134,12 @@ class DBMS
         }
     }
 
-    public void AggregateMax()
-    {
+    public void AggregateMax() {
         int iMax = 0;
         Student temp = null;
 
-        for(Student sref : lobj)
-        {
-            if(sref.Salary > iMax)
-            {
+        for (Student sref : lobj) {
+            if (sref.Salary > iMax) {
                 iMax = sref.Salary;
                 temp = sref;
             }
@@ -187,15 +149,12 @@ class DBMS
         temp.DisplayData();
     }
 
-    public void AggregateMin()
-    {
+    public void AggregateMin() {
         int iMin = (lobj.getFirst()).Salary;
         Student temp = lobj.getFirst();
 
-        for(Student sref : lobj)
-        {
-            if(sref.Salary < iMin)
-            {
+        for (Student sref : lobj) {
+            if (sref.Salary < iMin) {
                 iMin = sref.Salary;
                 temp = sref;
             }
@@ -205,64 +164,36 @@ class DBMS
         temp.DisplayData();
     }
 
-    public void AggregateSum()
-    {
+    public void AggregateSum() {
         long iSum = 0;
 
-        for(Student sref : lobj)
-        {
+        for (Student sref : lobj) {
             iSum = iSum + sref.Salary;
         }
 
-        System.out.println("Summation of salaries is : "+ iSum);
+        System.out.println("Summation of salaries is : " + iSum);
     }
 
-    public void AggregateAvg()
-    {
+    public void AggregateAvg() {
         long iSum = 0;
 
-        for(Student sref : lobj)
-        {
+        for (Student sref : lobj) {
             iSum = iSum + sref.Salary;
         }
 
-        System.out.println("Average salary is : "+ iSum / (lobj.size()));
+        System.out.println("Average salary is : " + iSum / (lobj.size()));
     }
 
-    public void AggregateCount()
-    {
-         System.out.println("Count is : "+lobj.size());
+    public void AggregateCount() {
+        System.out.println("Count is : " + lobj.size());
     }
 }
 
-class code101
-{
-    public static void main(String arg[])
-    {
+class code101 {
+    public static void main(String arg[]) {
         DBMS dobj = new DBMS();
 
         dobj.StartDBMS();
-
-        dobj.InsertData("Piyush",1000);
-        dobj.InsertData("Sakshi",3000);
-        dobj.InsertData("Sachin",2000);
-        dobj.InsertData("Suraj",10000);
-        dobj.InsertData("Atharva",8000);
-        dobj.InsertData("Supriya",9000);
-
-        dobj.DisplaySpecific(2);
-        dobj.DisplaySpecific("Piyush");
-
-        dobj.DeleteSpecific(2);
-        dobj.DeleteSpecific("Piyush");
-        dobj.DisplayAll();
-
-        dobj.AggregateMax();
-        dobj.AggregateMin();
-
-        dobj.AggregateSum();
-        dobj.AggregateAvg();
-        dobj.AggregateCount();
-
+        
     }
 }
